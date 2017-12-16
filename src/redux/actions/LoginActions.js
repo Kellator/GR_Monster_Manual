719574b1ff33c3fcf2e4c8e24d7711801ecc3e1b
Kellator <k.macleod21@gmail.com>
2017-12-16 18:00:26 -0500

    changed axios calls to use fetch

--- src/redux/actions/LoginActions.js
+++ src/redux/actions/LoginActions.js
@@ -1,48 +1,13 @@
 import axios from 'axios';
 import {DatabaseActions, ViewActions} from './index';
 import {SubmissionError} from 'redux-form';
-import {saveAuthToken, clearAuthToken} from '../local-storage';
+import {saveAuthToken, clearAuthToken} from '../../local-storage';
 import jwtDecode from 'jwt-decode';
 import {normalizeResponseErrors} from './utils';
 
 let url = 'http://localhost:5252/';
 // let url = "https://hidden-hamlet-10698.herokuapp.com/"
 console.log(url);
-
-
-// from https://auth0.com/blog/secure-your-react-and-redux-app-with-jwt-authentication/
-// export const LOGIN_REQUEST = 'LOGIN_REQUEST';
-// export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
-// export const LOGIN_FAILURE = 'LOGIN_FAILURE';
-
-// function requestLogin() {
-//     return {
-//         type: LOGIN_REQUEST,
-//         isFetching: true,
-//         isAuthenticated: false
-//     }
-// };
-
-// function receiveLogin(user) {
-//     return {
-//         type: LOGIN_SUCCESS,
-//         isFetching: false,
-//         isAuthenticated: true,
-//         user
-//     }
-// };
-
-// function loginError(message) {
-//     console.log(message);
-//     return {
-//         type: LOGIN_FAILURE,
-//         isFetching: false,
-//         isAuthenticated: false,
-//         message
-//     }
-// };
-
-
 
 export const SET_AUTH_TOKEN = 'SET_AUTH_TOKEN';
 export const setAuthToken = authToken => ({
@@ -71,41 +36,32 @@ export const authError = error => ({
     type: AUTH_ERROR,
     error
 });
+
 const storeAuthInfo = (authToken, dispatch) => {
     const decodedToken = jwtDecode(authToken);
     dispatch(setAuthToken(authToken));
     dispatch(authSuccess(decodedToken.user));
     saveAuthToken(authToken);
 };
+
 export const checkLogin = (values) => {
     return dispatch => {
         console.log("checking the user");
         dispatch(authRequest());
-        let username = values.username;
-        let password = values.password;
-        axios({
-            method: 'post',
-            url: url + 'auth/login', 
-            data: {
-                username,
-                password
-            }
+        fetch(url + 'auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: values.username,
+                password: values.password
+            })
         })
-        .then(response => normalizeResponseErrors(response))
-        .then(response => response.json())
+        .then(res => normalizeResponseErrors(res))
+        .then(res => res.json())
         .then(({authToken}) => storeAuthInfo(authToken, dispatch))
-        // .then(response => {
-        //     console.log(response);
-        //     let authToken = response.data.authToken;
-        //     const decodedToken = jwtDecode(authToken);
-        //     console.log(decodedToken);
-        //     if(response.status === 200) {
-        //         dispatch(authSuccess());
-        //         dispatch(setAuthToken(decodedToken));
-        //         dispatch(ViewActions.showHomeView());
-        //         localStorage.setItem('authToken', decodedToken);
-        //     }
-        // })
+        .then(dispatch(ViewActions.showHomeView()))
         .catch(error => {
             console.log(error);
             dispatch(authError(error));
@@ -113,48 +69,41 @@ export const checkLogin = (values) => {
         });
     }
 }
-// change to use axios - check url
-// export const refreshAuthToken = () => (dispatch, getState) => {
-//     dispatch(authRequest());
-//     const authToken = getState().auth.authToken;
-//     return fetch(`${API_BASE_URL}/auth/refresh`, {
-//         method: 'POST',
-//         headers: {
-//             // Provide our existing token as credentials to get a new one
-//             Authorization: `Bearer ${authToken}`
-//         }
-//     })
-//         .then(res => normalizeResponseErrors(res))
-//         .then(res => res.json())
-//         .then(({authToken}) => storeAuthInfo(authToken, dispatch))
-//         .catch(err => {
-//             // We couldn't get a refresh token because our current credentials
-//             // are invalid or expired, or something else went wrong, so clear
-//             // them and sign us out
-//             dispatch(authError(err));
-//             dispatch(clearAuth());
-//             clearAuthToken(authToken);
-//         });
-// };
+
+export const refreshAuthToken = () => (dispatch, getState) => {
+    dispatch(authRequest());
+    const authToken = getState().auth.authToken;
+    return fetch(url + 'auth/refresh', {
+        method: 'POST',
+        headers: {
+            // Provide our existing token as credentials to get a new one
+            Authorization: `Bearer ${authToken}`
+        }
+    })
+        .then(res => normalizeResponseErrors(res))
+        .then(res => res.json())
+        .then(({authToken}) => storeAuthInfo(authToken, dispatch))
+        .catch(err => {
+            // We couldn't get a refresh token because our current credentials
+            // are invalid or expired, or something else went wrong, so clear
+            // them and sign us out
+            dispatch(authError(err));
+            dispatch(clearAuth());
+            clearAuthToken(authToken);
+        });
+};
 export const LOGOUT_REQUEST = 'LOGOUT_REQUEST';
+export const requestLogout = () => {
+    type: LOGOUT_REQUEST
+};
 export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
+export const logoutSuccess = () => {
+    type: LOGOUT_SUCCESS
+};
 export const LOGOUT_FAILURE = 'LOGOUT_FAILURE';
-
-function requestLogout() {
-    return {
-        type: LOGOUT_REQUEST,
-        isFetching: true,
-        isAuthenticated: true
-    }
-}
-
-function receiveLogout() {
-    return {
-        type: LOGOUT_SUCCESS,
-        isFetching: false,
-        isAuthenticated: false
-    }
-}
+export const logoutFailure = () => {
+    type: LOGOUT_FAILURE
+};
 
 export const logoutUser = () => {
     return dispatch => {
@@ -202,9 +151,9 @@ export const createLogin = (values) => {
             url: url + 'user/', 
             data: values
         })
-        .then(response => {
-            console.log(response);
-            if(response.status === 201) {
+        .then(res => {
+            console.log(res);
+            if(res.status === 201) {
                 dispatch(receiveNewUser());
                 // needs to show a created new user page or message  - add messages to demo?
                 dispatch(ViewActions.showHomeView());
