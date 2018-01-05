a16c81c6a5c3f76304ff4f77fe451b59931c1cf7
Kellator <k.macleod21@gmail.com>
2018-01-04 23:02:27 -0500

    Added Jest and associated dev dependencies for testing.  BasicStats test file and CreatureCard test file added.

---
+++ src/components/card_view/BasicStats.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import BasicStats from './BasicStats';
+import renderer from 'react-test-renderer';
+import ReactDom from 'reacr-dom';
+
+// it('renders correctly', () => {
+//     const component = renderer
+//         .create(<BasticStats />)
+//         .toJson();
+//     expect(component).toMatchSnapshot();
+// });
