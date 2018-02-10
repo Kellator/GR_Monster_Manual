99458a766a2a278c9d1bee3671c874d3bc5a1465
Kellator <k.macleod21@gmail.com>
2018-02-10 15:30:37 -0500

    CardCreated component styled with buttons, spacing, and background color.

--- src/components/main/cardCreated.js
+++ src/components/main/cardCreated.js
@@ -1,18 +1,47 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
+import Grid from 'material-ui-next/Grid';
 
 // map state to props to display card name in <p>
 class CardCreated extends React.Component {
     render() {
-        return (
-            <div>
-                <h1>New Monster Card added to the database.</h1>
-                <p>You have added a new card to the database.</p>
-                <p>More cards to add?  Click the button below to add additional cards.</p>
-                <RaisedButton onClick={ this.props.create }>Add Another Card</RaisedButton>
-                <p>Finished entering cards?  Click the button below to return to the main page.</p>
-                <RaisedButton onClick={ this.props.home }>Home</RaisedButton>
-            </div>
+        return ( 
+            <Grid container justify="center">           
+                <Grid item xs={12} sm={8} md={6} className="align-center" >
+                    <div id="creation-confirmation" className="card-entry div-opaque-color" >
+                        <h1 className="form-container">New Creature Card added to the Codex.</h1>
+                        <h3 className="form-container">Click HOME to return to the search or click ADD MORE to add more creatures.</h3>
+                        <Grid container justify="space-around">
+                            <Grid item xs={6} >
+                                    <RaisedButton 
+                                        onClick={ this.props.home }
+                                        style={{
+                                            borderRadius: "1px",
+                                            fontWeight: "bold",
+                                            display: "block",
+                                            fontSize: "1.5rem"                   
+                                        }}
+                                    >Home</RaisedButton>
+                                </Grid>
+                            <Grid item xs={6} >
+                                <RaisedButton 
+                                    onClick={ this.props.create }
+                                    style={{
+                                        borderRadius: "1px",
+                                        fontWeight: "bold",
+                                        display: "block",
+                                        fontSize: "1.5rem"                  
+                                    }}
+                                    // buttonStyle={{
+                                    //     paddingLeft: "10px",
+                                    //     paddingRight: "10px"
+                                    // }}
+                                >Add More</RaisedButton>
+                            </Grid>
+                        </Grid>
+                    </div>
+                </Grid> 
+            </Grid>
         )
     }
 }
